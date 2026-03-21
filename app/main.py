0da64c31fd7fc718be3dfa041090ad03737f8e0b
Sudesh <sudeshkr861@gmail.com>
2026-03-21 17:10:31 +0530

    added backend api

--- app/main.py
+++ app/main.py
@@ -78,8 +78,6 @@ def create_task(task: TaskCreate):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-
-
 @app.get("/about",response_class = PlainTextResponse)
 def about_backend():
     return 'This is all about backend'
